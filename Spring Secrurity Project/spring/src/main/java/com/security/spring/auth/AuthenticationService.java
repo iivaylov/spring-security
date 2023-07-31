d1b96f075ddbc3bd8603135e7722dc77a5cbe74c
Ivaylo Iliev <ilievivaylo200@proton.me>
2023-07-31 15:04:23 +0300

    implementing register method

--- Spring Secrurity Project/spring/src/main/java/com/security/spring/auth/AuthenticationService.java
+++ Spring Secrurity Project/spring/src/main/java/com/security/spring/auth/AuthenticationService.java
@@ -1,14 +1,37 @@
 package com.security.spring.auth;
 
+import com.security.spring.config.JwtService;
+import com.security.spring.user.Role;
+import com.security.spring.user.User;
+import com.security.spring.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        User user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+
+        String jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
